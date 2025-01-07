ff4ba78aa1f30da13fa398e24b8b1087819346c9
ghdtmdals <seungmin.h9748@gmail.com>
2025-01-07 15:44:29 +0900

    Updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import keys
 from Trade.trade import Trader
 
 ### DB 로드와 같은 작업 수행
+### Database 폴더가 없으면 생성
 
 def main(*token):
     os.environ["UPBIT_OPEN_API_ACCESS_KEY"] = keys.UPBIT_ACCESS_KEY
@@ -19,7 +20,7 @@ def single_token_trade(token):
 
 def multi_token_trade(token): 
     ### multiprocessing으로 여러개 동시 실행
-    pass
+    print(token)
 
 if __name__ == "__main__":
     main('Bitcoin')
