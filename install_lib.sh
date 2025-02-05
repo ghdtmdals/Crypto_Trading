41de250f4f2c4a4eec1cb18378491d30cdea5c28
ghdtmdals <seungmin.h9748@gmail.com>
2025-02-05 20:38:55 +0900

    airflow update

--- install_lib.sh
+++ install_lib.sh
@@ -5,7 +5,7 @@ adduser --disabled-password --gecos "" user  \
         && adduser user sudo \
         && echo 'user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
-sudo apt-get install -y git vim
+sudo apt-get install -y git vim openssh-server
 
 pip install transformers PyJWT python-dateutil pymysql matplotlib ftfy regex tqdm git+https://github.com/openai/CLIP.git
 
@@ -13,15 +13,15 @@ pip install transformers PyJWT python-dateutil pymysql matplotlib ftfy regex tqd
 
 conda install -y scikit-learn
 
-AIRFLOW_VERSION=2.10.4
-PYTHON_VERSION="$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
-CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+# AIRFLOW_VERSION=2.10.4
+# PYTHON_VERSION="$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
+# CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
-airflow users create \
-    --username admin \
-    --firstname ${AIRFLOW_FIRSTNAME} \
-    --lastname ${AIRFLOW_LASTNAME} \
-    --role Admin \
-    --password ${AIRFLOW_PASSWORD} \
-    --email ${AIRFLOW_EMAIL}
+# airflow users create \
+#     --username admin \
+#     --firstname ${AIRFLOW_FIRSTNAME} \
+#     --lastname ${AIRFLOW_LASTNAME} \
+#     --role Admin \
+#     --password ${AIRFLOW_PASSWORD} \
+#     --email ${AIRFLOW_EMAIL}
