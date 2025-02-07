ab6b2e1eb206550ae5eb73e859f029152c3f4e66
ghdtmdals <seungmin.h9748@gmail.com>
2025-02-07 18:09:51 +0900

    dag updated

--- Data/News/coinpress.py
+++ Data/News/coinpress.py
@@ -4,6 +4,16 @@ from pytz import timezone
 from Data.News.news_crawling import NewsCrawling
 
 class CoinPress(NewsCrawling):
+    # @staticmethod
+    # def af_get_news_data(coin_name, token):
+    #     source = 'coinpress'
+    #     params = {"s": coin_name}
+    #     url = "https://coinspress.com/"
+    #     return super().af_get_news_data(token, source, url, params, \
+    #                                     title_block_tag = 'div', title_block_class_name = "col-12 order-3 order-md-2 col-md-4", \
+    #                                     title_tag = 'h2', title_class_name = "post-title", \
+    #                                     date_tag = 'li', date_class_name = "list-inline-item post-date")
+    
     def __init__(self, coin_name: str, token: str):
         self.coin_name = coin_name
         self.token = token
--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -3,6 +3,12 @@ from airflow.operators.bash import BashOperator
 
 from datetime import datetime
 
+with DAG("test_once", start_date = datetime(2025, 1, 1), schedule_interval = "@once", catchup = False) as dag:
+    test_once = BashOperator(
+        task_id = "test_once",
+        bash_command = 'echo Should Run Only Once'
+    )
+
 with DAG("test_dag", start_date = datetime(2025, 1, 1), schedule_interval = "@daily", catchup = False) as dag:
     test_task1 = BashOperator(
         task_id = "test_task1",
@@ -16,6 +22,7 @@ with DAG("test_dag", start_date = datetime(2025, 1, 1), schedule_interval = "@da
                         + '"cd /workspace && python ./airflow/airflow_test.py --test 2"'
     )
 
+    ### Error
     test_task3 = BashOperator(
         task_id = "test_task3",
         bash_command = 'ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@trader ' \
